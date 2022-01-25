d2f64621e72fd18311a32bde95d9ec8806729ce6
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-01-26 01:27:15 +0700

    update navbar

--- client/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ client/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ShipperService} from "../../services/shipper.service";
+import {Shipper} from "../../models/shipper";
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,10 +8,13 @@ import {ShipperService} from "../../services/shipper.service";
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
+  shipper = null as Shipper | null;
   constructor(private readonly shipperService: ShipperService) { }
 
   ngOnInit(): void {
+    this.shipperService.currentShipper$.subscribe(shipper =>{
+      this.shipper = shipper;
+    })
   }
   logout() {
     this.shipperService.logout();
