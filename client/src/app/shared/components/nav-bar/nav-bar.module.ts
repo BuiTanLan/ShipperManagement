a6432a3e83907d1fa046bcda4c175de973da0176
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-01-26 01:06:02 +0700

    refactor navbar

---
+++ client/src/app/shared/components/nav-bar/nav-bar.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import {NavBarComponent} from "./nav-bar.component";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavBarComponent
+  ]
+})
+export class NavBarModule { }
