3d646bbc258c94e329a6f5a581ea437abfbb6e2e
thuy <tranthiminhthuy0709@gmail.com>
2022-01-26 01:21:47 +0700

    -- history

--- client/src/app/history/history.component.ts
+++ client/src/app/history/history.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../shared/services/order.service';
+import { ShipperService } from '../shared/services/shipper.service';
+import { product } from '../shared/models/product';
+import {Order} from "../shared/models/order";
 
 @Component({
   selector: 'app-history',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  public  order: Order[]= [];
+  // public   orderDetail = {} as Order;
+     public   orderDetail: product[]= [];
+
+  public  user= '';
+  public OD =0;
+
+
+  constructor(
+    private common: OrderService,
+    private serverHttp: ShipperService  )
+     { }
 
   ngOnInit(): void {
+    this.serverHttp.getOrderId(1).subscribe((data)=>{
+      this.order = data;
+      console.log(this.order);
+
+    });
   }
 
+  public getOrDetail(order_id : number){
+      this.serverHttp.getOrderDetail(1).subscribe((data)=>{
+      this.orderDetail = data;
+      console.log(this.orderDetail);
+
+    });
+  }
 }
